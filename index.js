09cb84b645c77e3d0ebf5d5916284f76b48f45e6
diogopolesso <diogopolesso@gmail.com>
2022-11-30 16:00:43 -0300

    Quick Sort

---
+++ index.js
@@ -0,0 +1,45 @@
+const listaLivros = require('./array');
+
+function encontraMenores(pivo, array){
+    let menores = 0;
+
+    for(let atual = 0; atual < array.length; atual++){
+        let produtoAtual = array[atual];
+        if (produtoAtual.preco < pivo.preco){
+            menores++;
+        }
+    }
+
+    
+    trocaLugar(array, array.indexOf(pivo), menores);
+    return array;
+}
+
+function trocaLugar(array, de, para){
+    const elemento1 = array[de];
+    const elemento2 = array[para];
+
+    array[para] = elemento1;
+    array[de] = elemento2;
+}
+
+function divideNoPivo(array){
+    let pivo = array[Math.floor(array.length / 2)];
+    encontraMenores(pivo, array);
+    let valorMenores = 0;
+
+    for(let analisando = 0; analisando < array.length; analisando++){
+        let atual = array[analisando];
+        if (atual.preco <= pivo.preco && atual !== pivo){
+            trocaLugar(array, analisando, valorMenores)
+            valorMenores++;
+        }
+    }
+
+    return array;
+}
+
+//console.log(encontraMenores(listaLivros[2], listaLivros));
+//console.log(divideNoPivo(listaLivros));
+
+module.exports = trocaLugar
